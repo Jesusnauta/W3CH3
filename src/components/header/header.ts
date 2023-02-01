d121a4e2a6e9f724ef9650968b5113bf22783131
Jesus <jesus.cassano.2006@gmail.com>
2023-02-02 00:19:19 +0100

    header.ts changes

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -8,7 +8,7 @@ export class Header extends Component {
   constructor(public selector: string, public title: string) {
     super();
     this.template = this.createTemplate();
-    this.render('afterbegin');
+    this.render('beforebegin');
   }
 
   private createTemplate() {
@@ -17,7 +17,6 @@ export class Header extends Component {
       <h1>${this.title}</h1>
       <p role="generic" aria-label="info"></p>
       <p class="text"> 'TODOS MERECEN UNA FAMILIA'</p>
-
     </header>
     `;
   }
