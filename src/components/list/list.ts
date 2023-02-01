55429b2e337f1fb8b595bc8a68fb7481dfab7758
Jesus <jesus.cassano.2006@gmail.com>
2023-02-02 00:19:30 +0100

    list modify

---
+++ src/components/list/list.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { PetStructure } from '../../models/pet';
+import { Component } from '../component/component';
+
+import './list.scss';
+
+export class Pets extends Component {
+  constructor(public selector: string, public pets: PetStructure[]) {
+    super();
+    this.template = this.createTemplate();
+    this.render('beforebegin');
+  }
+
+  createTemplate() {
+    const items = this.pets
+      .map(
+        (item) =>
+          `<li>
+          <img src="./public/OIP (1).jfif" alt="dog" height="5px">
+          <div>ID: ${item.id}</div>
+          <div>Nombre del perro: ${item.name}</div>
+          <div>Raza: ${item.breed}</div>
+          <div>¿Adoptado?: ${item.isAdopted}</div>
+        </li>`
+      )
+      .join('\n');
+
+    return `
+      <section><ul>${items}</ul></section>
+    `;
+  }
+}
